41fb9bcef6490999a8b377aaa89ecf2e992eed2c
anupm12 <shuklaanupam18@gmail.com>
2020-12-19 15:32:31 +0530

    Initialize

---
+++ routes/tourRoutes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const tourController = require('./../controllers/tourControler');
+
+const routes = express.Router();
+
+routes.param('id', tourController.checkID);
+
+routes
+  .route('/')
+  .get(tourController.getAllTours)
+  .post(tourController.checkBody, tourController.createTour);
+routes
+  .route('/:id')
+  .get(tourController.getTour)
+  .patch(tourController.updateTour)
+  .delete(tourController.deleteTour);
+
+module.exports = routes;
